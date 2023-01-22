bcf44fec9fac0cd1d7862cef4b5554bdc3c99c3d
xcvbn23 <xcvbn23@fastmail.com>
2023-01-22 22:17:12 +0100

    feat: add replacement method

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from genetic_algorithm import GeneticAlgorithm
+from genetic_algorithm import REPLACEMENT_METHOD, GeneticAlgorithm
 
 
 class MyGeneticAlgorithm(GeneticAlgorithm):
     def gene_definition(self):
         return [(int, 0, 1), (int, -5, 10), (float, 10.5, 75.5)]
 
-    def fitness_func(self, gene: tuple) -> float:
+    def fitness_func(self, gene: list) -> float:
         return sum(gene)
 
     def generations(self):
-        return 100
+        return 1000
+
+    def mutation_rate(self) -> float:
+        return 0.05
 
     def num_of_parents(self):
         return 2
 
     def population_size(self):
-        return 10
+        return 1000
+
+    def replacement_method(self) -> REPLACEMENT_METHOD:
+        return REPLACEMENT_METHOD.WEAK_PARENT
 
 
 if __name__ == "__main__":
--- test_genetic_algorithm.py
+++ test_genetic_algorithm.py
@@ -1,8 +1,6 @@
-import random
-
 import pytest
 
-from genetic_algorithm import GeneticAlgorithm
+from genetic_algorithm import REPLACEMENT_METHOD, GeneticAlgorithm
 
 
 @pytest.fixture
@@ -17,11 +15,17 @@ def genetic_algorithm(random_seed):
         def generations(self):
             return 10
 
+        def mutation_rate(self) -> float:
+            return 0.25
+
         def num_of_parents(self):
             return 2
 
         def population_size(self):
             return 10
+
+        def replacement_method(self) -> REPLACEMENT_METHOD:
+            return REPLACEMENT_METHOD.RANDOM
 
     genetic_algorithm = MyGeneticAlgorithm()
     yield genetic_algorithm
@@ -31,21 +35,22 @@ class TestGeneticAlgorithm:
     def test_initialise_population(self, genetic_algorithm):
         assert len(genetic_algorithm.individuals) == 10
         assert genetic_algorithm.individuals == [
-            ([0, -5, 58.70078248438914], 53.70078248438914),
-            ([0, 2, 19.569965354134354], 21.569965354134354),
-            ([0, -3, 48.88201330918758], 45.88201330918758),
-            ([0, -5, 16.59019059100351], 11.590190591003509),
-            ([0, -5, 46.98092909100984], 41.98092909100984),
-            ([1, 2, 39.698588008450486], 42.698588008450486),
-            ([1, -5, 59.822478863434874], 55.822478863434874),
-            ([0, 8, 32.61628357366947], 40.61628357366947),
-            ([0, 1, 72.71884969344077], 73.71884969344077),
-            ([1, -2, 16.528479819709613], 15.528479819709613),
+            (0, [0, -5, 58.70078248438914], 53.70078248438914),
+            (1, [0, 2, 19.569965354134354], 21.569965354134354),
+            (2, [0, -3, 48.88201330918758], 45.88201330918758),
+            (3, [0, -5, 16.59019059100351], 11.590190591003509),
+            (4, [0, -5, 46.98092909100984], 41.98092909100984),
+            (5, [1, 2, 39.698588008450486], 42.698588008450486),
+            (6, [1, -5, 59.822478863434874], 55.822478863434874),
+            (7, [0, 8, 32.61628357366947], 40.61628357366947),
+            (8, [0, 1, 72.71884969344077], 73.71884969344077),
+            (9, [1, -2, 16.528479819709613], 15.528479819709613),
         ]
 
     def test_run(self, genetic_algorithm):
         genetic_algorithm.run()
         assert genetic_algorithm.individuals[0] == (
-            [1, -4, 58.17889313665809],
-            55.17889313665809,
+            6,
+            [1, -5, 59.822478863434874],
+            55.822478863434874,
         )

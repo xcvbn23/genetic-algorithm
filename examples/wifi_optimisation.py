87ca53e3a8009c4e10b026d90d91e5bebdea9613
xcvbn23 <123225579+xcvbn23@users.noreply.github.com>
2023-01-27 23:32:56 +0100

    feat: add simple wall attenuation to wifi optimisation example (#7)

--- examples/wifi_optimisation.py
+++ examples/wifi_optimisation.py
@@ -1,135 +1,16 @@
 import math
 import statistics
+
 import pygame
 
-from genetic_algorithm import GeneticAlgorithm, REPLACEMENT_METHOD
+from examples.wifi_optimisation_utils import (
+    freespace_propagation_loss,
+    segment_intersect,
+)
+from genetic_algorithm import REPLACEMENT_METHOD, GeneticAlgorithm
 
-
-def slope(p1, p2):
-    return (p2[1] - p1[1]) * 1.0 / (p2[0] - p1[0])
-
-
-def y_intercept(slope, p1):
-    return p1[1] - 1.0 * slope * p1[0]
-
-
-def intersect(line1, line2):
-    min_allowed = 1e-5  # guard against overflow
-    big_value = 1e10  # use instead (if overflow would have occurred)
-    m1 = slope(line1[0], line1[1])
-    print("m1: %d" % m1)
-    b1 = y_intercept(m1, line1[0])
-    print("b1: %d" % b1)
-    m2 = slope(line2[0], line2[1])
-    print("m2: %d" % m2)
-    b2 = y_intercept(m2, line2[0])
-    print("b2: %d" % b2)
-    if abs(m1 - m2) < min_allowed:
-        x = big_value
-    else:
-        x = (b2 - b1) / (m1 - m2)
-    y = m1 * x + b1
-    y2 = m2 * x + b2
-    print("(x,y,y2) = %d,%d,%d" % (x, y, y2))
-    return (int(x), int(y))
-
-
-def segment_intersect(line1, line2):
-    # https://www.codeproject.com/Tips/864704/Python-Line-Intersection-for-Pygame
-    intersection_pt = intersect(line1, line2)
-
-    print(line1[0][0], line1[1][0], line2[0][0], line2[1][0], intersection_pt[0])
-    print(line1[0][1], line1[1][1], line2[0][1], line2[1][1], intersection_pt[1])
-
-    if line1[0][0] < line1[1][0]:
-        if intersection_pt[0] < line1[0][0] or intersection_pt[0] > line1[1][0]:
-            print("exit 1")
-            return None
-    else:
-        if intersection_pt[0] > line1[0][0] or intersection_pt[0] < line1[1][0]:
-            print("exit 2")
-            return None
-
-    if line2[0][0] < line2[1][0]:
-        if intersection_pt[0] < line2[0][0] or intersection_pt[0] > line2[1][0]:
-            print("exit 3")
-            return None
-    else:
-        if intersection_pt[0] > line2[0][0] or intersection_pt[0] < line2[1][0]:
-            print("exit 4")
-            return None
-
-    return intersection_pt
-
-
-pygame.init()
-pygame.display.set_caption("Line of Sight")
-display_win = pygame.display.set_mode([500, 500])
-win = pygame.Surface((100, 100))
-
-
-WHITE = (200, 200, 200)
-GREY = (50, 50, 50)
-BLACK = (0, 0, 0)
-RED = (200, 0, 0)
-GREEN = (0, 255, 0)
-TAN = (240, 171, 15)
-YELLOW = (255, 255, 0)
-
-
-class Being(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int, colour=YELLOW, size=5):
-        pygame.sprite.Sprite.__init__(self)
-        self.colour = colour
-        self.image = pygame.Surface((size, size), pygame.SRCALPHA)
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.size = size
-        self.seen = False
-        self.update()
-
-    def update(self):
-        """If we've been seen, go red with embrassment"""
-        if self.seen:
-            colour = RED
-        else:
-            colour = self.colour
-        pygame.draw.circle(
-            self.image, colour, (self.size // 2, self.size // 2), self.size // 2
-        )
-
-    def setSeen(self, seen=True):
-        self.seen = seen
-
-    def getCentre(self):
-        return self.rect.center
-
-    def getRect(self):
-        return self.rect
-
-
-# Users
-users = [(0, 0), (100, 100)]
-user_sprites = pygame.sprite.Group()
-for user_location in users:
-    user_sprites.add(Being(*user_location, GREEN))
-user_sprites.update()
-user_sprites.draw(win)
-
-# Routers
-player_sprite = pygame.sprite.GroupSingle()
-player = Being(0, 0)  # bottom middle
-player_sprite.add(player)
-
-# Walls
-walls = [[(75, 50), (100, 50)]]
-
-
-def freespace_propagation_loss(distance: float, frequency: float):
-    # frequency is frequency(Hz)
-    # distance is meters(m)
-    light_speed = 300000000.0  # m/s
-    return 20 * math.log10((4.0 * math.pi * distance) / (light_speed / frequency))
+users = [(0, 50), (100, 50)]
+walls = [[(75, 0), (75, 75)]]
 
 
 class WifiOptimisationGeneticAlgorithm(GeneticAlgorithm):
@@ -138,9 +19,16 @@ class WifiOptimisationGeneticAlgorithm(GeneticAlgorithm):
 
     def fitness_func(self, gene: list) -> float:
         path_losses = []
+
+        router = gene[1:]
+
         for user in users:
-            distance = math.dist(user, gene[1:])
+            distance = math.dist(user, router)
             path_loss = freespace_propagation_loss(distance, 2.412 * math.pow(10, 9))
+            for wall in walls:
+                if segment_intersect(wall, [router, user]):
+                    path_loss += 3
+
             if path_loss > 85:
                 return -999
             path_losses.append(path_loss)
@@ -164,26 +52,64 @@ class WifiOptimisationGeneticAlgorithm(GeneticAlgorithm):
         _, best_gene, _ = best_individuals[0]
         [_, router_x, router_y] = best_gene
 
+        pygame.init()
+        pygame.display.set_caption("Wifi Optimisation Algorithm")
+        display_win = pygame.display.set_mode([200, 200])
+        win = pygame.Surface((100, 100))
+
+        WHITE = (200, 200, 200)
+        GREY = (50, 50, 50)
+        BLACK = (0, 0, 0)
+        RED = (200, 0, 0)
+        GREEN = (0, 255, 0)
+        TAN = (240, 171, 15)
+        YELLOW = (255, 255, 0)
+
+        class Unit(pygame.sprite.Sprite):
+            def __init__(self, x: int, y: int, colour=YELLOW, size=5):
+                pygame.sprite.Sprite.__init__(self)
+                self.colour = colour
+                self.image = pygame.Surface((size, size), pygame.SRCALPHA)
+                self.rect = self.image.get_rect()
+                self.rect.center = (x, y)
+                self.size = size
+                self.seen = False
+                pygame.draw.circle(
+                    self.image, colour, (self.size // 2, self.size // 2), self.size // 2
+                )
+
         win.fill(BLACK)
 
+        user_sprites = pygame.sprite.Group()
+        for user_location in users:
+            user_sprites.add(Unit(*user_location, GREEN))
+        user_sprites.update()
+        user_sprites.draw(win)
         user_sprites.draw(win)
 
-        player.rect.x = router_x
-        player.rect.y = router_y
-        player_sprite.draw(win)
+        router_sprite = pygame.sprite.GroupSingle()
+        router = Unit(0, 0)
+        router_sprite.add(router)
+        router.rect.x = router_x
+        router.rect.y = router_y
+        router_sprite.draw(win)
 
         for wall in walls:
             pygame.draw.line(win, TAN, *wall)
 
         for user in users:
             for wall in walls:
-                router_user = [(router_x, router_y), user]
-                print(segment_intersect(wall, router_user))
+                router_user = [(round(router_x), round(router_y)), user]
                 pygame.draw.line(win, RED, *router_user)
 
         scaled_win = pygame.transform.smoothscale(win, display_win.get_size())
         display_win.blit(scaled_win, (0, 0))
         pygame.display.flip()
+
+        pygame.image.save(
+            display_win, "./examples/wifi_optimisation_screenshots/plan.png"
+        )
+        pygame.quit()
 
     def population_size(self):
         return 100
@@ -195,5 +121,3 @@ class WifiOptimisationGeneticAlgorithm(GeneticAlgorithm):
 if __name__ == "__main__":
     genetic_algorithm = WifiOptimisationGeneticAlgorithm()
     genetic_algorithm.run()
-    pygame.image.save(display_win, "screenshot.jpg")
-    pygame.quit()
